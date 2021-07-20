9e97dcec8bafabe97c3d8abdc27814c1b2c37614
corlys <duscae047@gmail.com>
2021-07-20 09:14:38 +0700

    add some styling to the list and fix the grid

--- component/List.tsx
+++ component/List.tsx
@@ -5,13 +5,13 @@ const List: React.FC<IDatetime> = ({ times, date }) => {
   // console.log(date);
   return (
     <div className="w-full">
-      <div className="grid grid-cols-12 gap-4">
+      <div className="grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-8 p-8 gap-4">
         {Object.entries(times).map(([key, value]) => (
-          <div
-            key-={key}
-            className="container border-2 border-gray-600 hover:border-indigo-500"
-          >
-            {`Waktu ${key}:${value}`}
+          <div className="rounded-xl shadow-xl" key={key}>
+            <div className="flex flex-col p-2 border border-indigo-600 rounded-xl items-center">
+              <div className="uppercase text-indigo-500 font-bold text-2xl">{key}</div>
+              <div className="text-indigo-500 font-bold text-xl">{value}</div>
+            </div>
           </div>
         ))}
       </div>

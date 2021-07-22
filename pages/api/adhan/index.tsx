c52cc914772745193e5fd3683dfc1be654bb9898
corlys <duscae047@gmail.com>
2021-07-22 14:20:37 +0700

    fetch error handling

--- pages/api/adhan/index.tsx
+++ pages/api/adhan/index.tsx
@@ -2,11 +2,18 @@
 import { IDatetime } from "../../../model/PrayerAPI";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req: NextApiRequest, res: NextApiResponse<IDatetime>) => {
+export interface ErrorResponse {
+  err: string;
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<IDatetime | ErrorResponse>
+) => {
   try {
     const city = req.query.city;
     const call = await fetch(
-      `http://api.pray.zone/v2/times/today.json?city=${city}`
+      `https://api.pray.zone/v2/times/today.json?city=${city}`
     );
 
     const {
@@ -16,6 +23,6 @@ export default async (req: NextApiRequest, res: NextApiResponse<IDatetime>) => {
       .status(200)
       .json({ times: datetime[0].times, date: datetime[0].date });
   } catch (error) {
-    return res.status(400);
+    return res.status(400).json({ err: error.toString() });
   }
 };
